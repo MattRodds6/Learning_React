6bbfae45f08bc02034f3c77050781f0dbb0a20e5
Mateo Rodriguez Reyes <38114025+MattRodds6@users.noreply.github.com>
2025-02-02 18:54:58 -0800

    correcting the dialog modal functionality

--- 05-Countdown_App/src/components/TimerChallenge.jsx
+++ 05-Countdown_App/src/components/TimerChallenge.jsx
@@ -1,7 +1,9 @@
 import { useState, useRef } from "react";
 import ResultModal from "./ResultModal";
+
 export default function TimerChallenge({title, targetTime}) {
     const timer = useRef();
+    const dialog = useRef();
     const [timerExpired, setTimerExpired] = useState(false);
     const [timerStarted, setTimerStarted] = useState(false);
 
@@ -9,6 +11,7 @@ export default function TimerChallenge({title, targetTime}) {
         setTimerStarted(true);
         timer.current = setTimeout(() => {
             setTimerExpired(true);
+            dialog.current.showModal();
         }, targetTime * 1000);
     }
 
@@ -20,7 +23,7 @@ export default function TimerChallenge({title, targetTime}) {
 
     return (
         <>
-            {timerExpired && <ResultModal result="Lost" targetTime={targetTime} />}
+            {timerExpired && <ResultModal ref={dialog} result="Lost" targetTime={targetTime} />}
             <section className="challenge">
                 <h2>{title}</h2>
                 <p className="challenge-time">
