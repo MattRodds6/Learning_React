48bc5277bd7d79e47aead9718808fc28e4ed5be5
Mateo Rodriguez Reyes <MattRodds6@gmail.com>
2025-02-20 08:22:57 -0800

    added temp body

--- 06-Project_Management_App/src/App.jsx
+++ 06-Project_Management_App/src/App.jsx
@@ -2,6 +2,12 @@ function App() {
   return (
     <>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <body>
+        //here is the "side bar project list" of the app
+        <li>
+          
+        </li>
+      </body>
     </>
   );
 }
